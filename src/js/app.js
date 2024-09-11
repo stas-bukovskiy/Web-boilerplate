d00786ec576f02aa6a9762c29b9378f3119d3977
stas-bk <s.bukovskyi@aumify.io>
2024-09-11 20:59:15 +0300

    compete Lab1

--- src/js/app.js
+++ src/js/app.js
@@ -1,6 +1,27 @@
 const testModules = require('./test-module');
 require('../css/app.css');
 
-/** ******** Your code here! *********** */
+function openPopup(popupId) {
+  document.getElementById(popupId).style.display = 'flex';
+}
+
+document.querySelectorAll('.add-teacher-btn').forEach((btn) => {
+  btn.addEventListener('click', () => {
+    openPopup('add-teacher-popup');
+  });
+});
+
+document.querySelectorAll('.profile-photo').forEach((btn) => {
+  btn.addEventListener('click', () => {
+    openPopup('teacher-info-popup');
+  });
+});
+
+document.querySelectorAll('.close-btn').forEach((btn) => {
+  btn.addEventListener('click', () => {
+    document.getElementById('add-teacher-popup').style.display = 'none';
+    document.getElementById('teacher-info-popup').style.display = 'none';
+  });
+});
 
 console.log(testModules.hello);
